c019a663cd286e9e1129a67b84999993d7c2f26f
Tham <zarub2k@gmail.com>
2019-10-22 07:25:20 +0530

    back ground color is changed

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
+          backgroundColor: Colors.blueGrey,
           appBar: AppBar(
             title: Text('I Am Rich'),
             backgroundColor: Colors.blueGrey[900],
